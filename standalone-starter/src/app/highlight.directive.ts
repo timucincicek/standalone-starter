e94f85a5697a83f3fb0cc7aa9e9f2a7f99a5c80e
Timuçin Çiçek <39439504+timucincicek@users.noreply.github.com>
2023-03-27 10:23:17 +0300

    Add files via upload

---
+++ standalone-starter/src/app/highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private readonly el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('white');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+}
